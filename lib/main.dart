dcb6916ee16c232b7d45135c9255f42e8e68d961
Kaike <kaikedias98@gmail.com>
2024-03-20 20:40:08 -0300

    refactor: change supabase initialize method to use .env file and change location for date formatting

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_config/flutter_config.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:supabase_auth_ui/supabase_auth_ui.dart';
 import 'package:vagalivre/modules/auth/pages/login_page.dart';
-import 'package:vagalivre/modules/auth/pages/splash_page.dart';
+import 'package:vagalivre/modules/auth/pages/register_user_info_page.dart';
+import 'package:vagalivre/modules/home/pages/home_page.dart';
+import 'package:vagalivre/theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await FlutterConfig.loadEnvVariables();
+  initializeDateFormatting('pt_BR', null);
 
   await Supabase.initialize(
-    url: 'YOUR_SUPABASE_URL',
-    anonKey: 'YOUR_SUPABASE_ANON_KEY',
+    url: FlutterConfig.get('PUBLIC_SUPABASE_URL'),
+    anonKey: FlutterConfig.get('PUBLIC_SUPABASE_ANON_KEY'),
   );
 
   runApp(const MyApp());
@@ -22,11 +28,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const LoginPage(),
+      theme: MaterialTheme(ThemeData.light().textTheme).light(),
+      initialRoute: "PersonalInformation",
+      routes: {
+        "/": (context) => const HomePage(),
+        "login": (context) => const LoginPage(),
+        "PersonalInformation": (context) => RegisterUserInfoPage(),
+      },
     );
   }
 }
