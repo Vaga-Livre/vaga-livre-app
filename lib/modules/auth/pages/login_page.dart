8236e26c4e21d57d6250ed6ee236cc0e6332042c
Igor Julliano <igor.julliano2@gmail.com>
2024-05-21 01:28:46 -0300

    fix: 🎨 notification bar color

--- lib/modules/auth/pages/login_page.dart
+++ lib/modules/auth/pages/login_page.dart
@@ -19,90 +19,90 @@ class _LoginPageState extends State<LoginPage> {
     const spaceDefault = SizedBox.square(
       dimension: 16,
     );
+    final notificationBarSpace = MediaQuery.viewPaddingOf(context).top;
 
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                color: context.colorScheme.primary,
-                height: 127 + 50,
-                child: Center(
-                  child: Hero(
-                    tag: "splash-image-login",
-                    child: Image.asset(
-                      'assets/images/darker_expanded.png',
-                      height: 127,
-                      fit: BoxFit.cover,
-                    ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 127 + 50 + notificationBarSpace,
+              color: context.colorScheme.primary,
+              padding: EdgeInsets.only(top: notificationBarSpace),
+              child: Center(
+                child: Hero(
+                  tag: "splash-image-login",
+                  child: Image.asset(
+                    'assets/images/darker_expanded.png',
+                    height: 127,
+                    fit: BoxFit.cover,
                   ),
                 ),
               ),
-              spaceDefault,
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: Column(
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Text.rich(
-                          style: context.textTheme.headlineSmall,
-                          const TextSpan(
-                            children: [
-                              TextSpan(text: "Encontre os\n"),
-                              TextSpan(
-                                text: "melhores estacionamentos\n",
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                              TextSpan(text: "perto do seu destino"),
-                            ],
-                          ),
+            ),
+            spaceDefault,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text.rich(
+                        style: context.textTheme.headlineSmall,
+                        const TextSpan(
+                          children: [
+                            TextSpan(text: "Encontre os\n"),
+                            TextSpan(
+                              text: "melhores estacionamentos\n",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            TextSpan(text: "perto do seu destino"),
+                          ],
                         ),
-                        const SizedBox.square(
-                          dimension: 32,
-                        ),
-                        Text(
-                          "Entre agora e aproveite",
-                          style: context.textTheme.bodyLarge,
-                        )
-                      ],
-                    ),
-                    spaceDefault,
-                    Column(
-                      children: [
-                        // Create a Email sign-in/sign-up form
-                        SupaEmailAuth(
-                          onSignInComplete: (response) {
-                            checkUserMetadataAndNavigate(response, context);
-                          },
-                          onSignUpComplete: (response) {
-                            checkUserMetadataAndNavigate(response, context);
-                          },
-                        ),
-                        spaceDefault
-                      ],
-                    ),
-                  ],
-                ),
+                      ),
+                      const SizedBox.square(
+                        dimension: 32,
+                      ),
+                      Text(
+                        "Entre agora e aproveite",
+                        style: context.textTheme.bodyLarge,
+                      )
+                    ],
+                  ),
+                  spaceDefault,
+                  Column(
+                    children: [
+                      // Create a Email sign-in/sign-up form
+                      SupaEmailAuth(
+                        onSignInComplete: (response) {
+                          checkUserMetadataAndNavigate(response, context);
+                        },
+                        onSignUpComplete: (response) {
+                          checkUserMetadataAndNavigate(response, context);
+                        },
+                      ),
+                      spaceDefault
+                    ],
+                  ),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 48, vertical: 16),
-                child: Text.rich(
-                  style: context.textTheme.bodyLarge,
-                  textAlign: TextAlign.center,
-                  const TextSpan(children: [
-                    TextSpan(text: "Ao entrar no Vaga Livre, você concorda com os nossos "),
-                    TextSpan(
-                      text: "Termos e Política de Privacidade",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ]),
-                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 48, vertical: 16),
+              child: Text.rich(
+                style: context.textTheme.bodyLarge,
+                textAlign: TextAlign.center,
+                const TextSpan(children: [
+                  TextSpan(text: "Ao entrar no Vaga Livre, você concorda com os nossos "),
+                  TextSpan(
+                    text: "Termos e Política de Privacidade",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ]),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
