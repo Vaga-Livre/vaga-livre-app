601c872bac26d8da0b6f8fa699e121d33f1e0e35
Igor Julliano <igor.julliano2@gmail.com>
2024-05-23 02:19:31 -0300

    fix: check current logged user on splash page

--- lib/modules/auth/pages/splash_page.dart
+++ lib/modules/auth/pages/splash_page.dart
@@ -1,6 +1,7 @@
 import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../../config/extension.dart';
 
@@ -11,7 +12,9 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return FlutterSplashScreen(
       asyncNavigationCallback: () async {
-        Future.delayed(Durations.extralong4).then((value) => context.go("/login"));
+        final loggedIn = Supabase.instance.client.auth.currentSession != null;
+
+        Future.delayed(Durations.extralong4).then((value) => context.go(loggedIn ? "/" : "/login"));
       },
       splashScreenBody: Container(
         color: context.colorScheme.primary,
