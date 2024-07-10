27e7889c1a3ab35c9ae710d7bef6e5ca808bcd26
Igor Julliano <igor.julliano2@gmail.com>
2024-07-10 10:34:34 -0300

    chore: add TODO item @KaikeDias

--- lib/modules/park/services/park_slots_service.dart
+++ lib/modules/park/services/park_slots_service.dart
@@ -2,10 +2,11 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 class ParkSlotsService {
   ParkSlotsService();
-  
+
   final _supabaseClient = Supabase.instance.client;
 
   Future<void> reserveSlot(String parkId) async {
+    // TODO(KaikeDias): implementar função para criar uma reserva
     final response = await _supabaseClient.from('park_slots').insert(
       {'park_id': parkId, 'reserved': true},
     );
