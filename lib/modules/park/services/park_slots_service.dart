5f291e25cdcf5281bd15b2fe2521d891557ca976
Kaike <kaikedias98@gmail.com>
2024-07-18 15:11:18 -0300

    feat: add create reservation function

--- lib/modules/park/services/park_slots_service.dart
+++ lib/modules/park/services/park_slots_service.dart
@@ -1,14 +1,24 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:vagalivre/modules/park/enums/payment_method.dart';
 
 class ParkSlotsService {
   ParkSlotsService();
 
   final _supabaseClient = Supabase.instance.client;
 
-  Future<void> reserveSlot(String parkId) async {
-    // TODO(KaikeDias): implementar função para criar uma reserva
-    final response = await _supabaseClient.from('park_slots').insert(
-      {'park_id': parkId, 'reserved': true},
-    );
+  Future<void> reserveSlot(int parkId, TimeOfDay beginTime, TimeOfDay endTime,
+      PaymentMethod payment) async {
+    final formattedStartTime = "${beginTime.hour}:${beginTime.minute}:00";
+    final formattedEndTime = "${endTime.hour}:${endTime.minute}:00";
+
+    final response = await _supabaseClient.rpc('create_reservation', params: {
+      'park_id': parkId,
+      'start_time': formattedStartTime,
+      'end_time': formattedEndTime,
+      'payment_type_id': 1
+    });
   }
 }
